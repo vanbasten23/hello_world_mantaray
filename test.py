c053d00a26ff2743423eac4223e358d8ec356d5d
Xiongfei Wei <isaacwxf23@gmail.com>
2024-08-28 17:07:54 +0000

    add a test script.

---
+++ test.py
@@ -0,0 +1,12 @@
+"""
+Test script to be used.
+"""
+
+
+
+def main() -> None:
+  print('xw32 hello_world_mantaray.test.main is called')
+
+
+if __name__ == "__main__":
+  print('xw32 hello_world_mantaray.test is called')
